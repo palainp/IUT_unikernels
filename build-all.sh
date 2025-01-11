a3d73a07969c9e41c5dc02496654ec36f5325082
Pierre Alain <pierre.alain@tuta.io>
2025-01-11 10:44:44 +0100

    nat configures its interfaces itself with DHCP

--- build-all.sh
+++ build-all.sh
@@ -5,8 +5,8 @@ set -e
 opam pin | grep solo5 || opam opam pin solo5 git@github.com:palainp/solo5.git#vga -y
 opam pin | grep tcpip || opam pin git@github.com:palainp/mirage-tcpip.git#add-default-route -y
 
-# unikernels that relies on DHCP for interface configuration
-for UNIKERNEL in website nat dns-resolver ; do
+# unikernels that relies on DHCP for interface configuration (via the generic_stackv4v6 call)
+for UNIKERNEL in website dns-resolver ; do
     printf -- "---\n- Compilation of %s\n---\n" $UNIKERNEL
     cd $UNIKERNEL
     mirage configure -t virtio --dhcp=true && make depend && dune build && cp dist/$UNIKERNEL.virtio ../image
@@ -16,8 +16,8 @@ for UNIKERNEL in website nat dns-resolver ; do
     cd ..
 done
 
-# special net unikernel (not using DHCP for interface configuration)
-for UNIKERNEL in dhcp simple-fw ; do
+# special net unikernel (not using DHCP for interface configuration, directly specify it)
+for UNIKERNEL in dhcp simple-fw nat ; do
     printf -- "---\n- Compilation of %s\n---\n" $UNIKERNEL
     cd $UNIKERNEL
     mirage configure -t virtio && make depend && dune build && cp dist/$UNIKERNEL.virtio ../image
